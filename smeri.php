5aaf089ff24b2b53fabec8d611aebe9d5fd80210
florjan2006 <sflatinek@gmail.com>
2024-09-17 08:38:56 +0200

    Update smeri.php

--- smeri.php
+++ smeri.php
@@ -1,1 +1,9 @@
 <?php include("glava.php"); ?>
+
+<!--Body Content-->
+
+
+<p class="smeritext">Elektrotehnik</p>
+<p class="smeritext">Kemijski tehnik</p>
+<p class="smeritext">Tehnik racunalnistva</p>
+<p class="smeritext">Elektikar</p>
