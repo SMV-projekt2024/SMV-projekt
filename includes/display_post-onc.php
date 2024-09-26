cca8c64394e0d9d847e11e0299ea6b8a2049a702
LukaJ71 <163745815+LukaJ71@users.noreply.github.com>
2024-09-26 18:13:59 +0200

    Prikaz naloge na predmet strani

--- includes/display_post-onc.php
+++ includes/display_post-onc.php
@@ -6,8 +6,9 @@ if (isset($_GET["id"])){
     require_once "database-inc.php";
     require_once "functions-inc.php";
 
-    $sql = "SELECT * FROM Predmeti
-    WHERE id_predmet = $id_predmeta";
+    $sql = "SELECT * FROM Predmeti JOIN Naloge 
+    ON Predmeti.id_predmet = Naloge.id_predmet
+    WHERE Predmeti.id_predmet = $id_predmeta";
     $statement = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($statement, $sql)){
         header("location: ../prva_stran.php?error=StatementFailed");
@@ -30,7 +31,32 @@ if (isset($_GET["id"])){
         echo '</div>';
         echo '<div class="PostBox">';
         echo '    <div class="innerPostBox">';
-        echo        $row["celo_ime"];
+       
+        
+        echo '<div class="post">';
+        echo '<a href="post.php?id=' . $row["id_naloga"] . '">';
+        //echo '<img class="postImg" src="' . $row["PostsImgUrl"] . '">';
+        echo '<div class="postBody">';
+        echo '<h2 class="postTitle">' . $row["naslov"] . '</h2>';
+        echo '<p class="postText">' . $row["opis"] . '</p> <br>';
+        //echo '<p class="postText">' . $row["PostsDescription"] . '</p> <br>';
+        //echo '<p class="postInfo">Created by <i>' . $row["UsersUsername"] . '</i></p>';
+        //echo '<p class="postInfo">Published on <i>' . $row["PostsDate"] . '</i></p>';
+
+        /*
+        if ($currentUser == $row['UsersUsername'] || roleCheck() == "admin") {
+            echo "<a class='deleteButton' href='includes\delete_data-inc.php?id=" .$row["PostsId"] . "&type=post' ><img class='deleteIcon' src='img\delete-removebg-Outside.png' onmouseover='change(this)' onmouseout='changeback(this)'></a>";
+        }
+            */
+        
+        echo '</a>';
+        echo '</div>';
+        echo '</div>';
+
+
+
+
+
         echo '    </div>';
         echo '</div>';
 
