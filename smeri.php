2c27403d1c7a4ef5a30f2487af36fffea0894e6b
LukaJ71 <163745815+LukaJ71@users.noreply.github.com>
2024-10-01 08:41:33 +0200

    Sole stran

--- smeri.php
+++ smeri.php
@@ -3,20 +3,38 @@
 
 <!--Body Content-->
 
-<div class="SmerimainContainer">
-    <div class="smeriContainer">
-        <p>Elektrotehnik</p>
-    </div>
-    <div  class="smeriContainer">
-        <p>Kemijski tehnik</p>
-    </div>
-    <div class="smeriContainer">
-        <p>Tehnik racunalnistva</p>
-    </div>
-    <div class="smeriContainer">
-    <p>Elektikar</p>
+<div class="mainContainer">
+    <div class="postsContainer">
+            <div class="postBody, hidden">
+                <a href="post.php?id=1">
+                    <h2 class="postTitle">Srednja šola za kemijo, elektrotehniko in računalništvo</h2>
+                    <p class="postText">KER verjamemo</p> <br>
+                </a>
+            </div> 
+            
+            <div class="postBody, hidden">
+                <a href="post.php?id=1">
+                    <h2 class="postTitle">Srednja šola za gradbeništvo in varovanje okolja</h2>
+                    <p class="postText">GOVno</p> <br>
+                </a>
+            </div>
+
+            <div class="postBody, hidden">
+                <a href="post.php?id=1">
+                    <h2 class="postTitle">Srednja šola za strojništvo, mehatroniko in medije</h2>
+                    <p class="postText">Gskjdhfkjshhf</p> <br>
+                </a>
+            </div>
+
+            <div class="postBody, hidden">
+                <a href="post.php?id=1">
+                    <h2 class="postTitle">Srednja šola za logistiko in stritvene dejavnosi</h2>
+                    <p class="postText">Kidrič</p> <br>
+                </a>
+            </div>
     </div>
 </div>
+        
 
 
 
